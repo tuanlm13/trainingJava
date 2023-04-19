02ecd7af39d91abefe2b612a8b47c95a2ee58e10
tuanlm13 <minhtuanna.haui@gmail.com>
2023-04-19 22:53:52 +0700

    java basic 02

--- src/javaBasic/SinhVien.java
+++ src/javaBasic/SinhVien.java
@@ -1,4 +1,4 @@
-package baiTap;
+package javaBasic;
 
 public class SinhVien {
 
---
+++ src/javaBasic/practicesDraft.java
@@ -0,0 +1,15 @@
+package javaBasic;
+
+public class practicesDraft {
+    static{
+        //block
+        //auto run
+        // bloccccck
+    }
+
+    public static int number=90;
+
+    public static void main(String[] args) {
+        System.out.println(number);
+    }
+}
